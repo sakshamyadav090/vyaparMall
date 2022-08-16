158259408c53ff0bf542927cd2f5368e2211de94
Saksham Kumar <saksham.kumar@acsicorp.com>
2022-08-16 10:28:30 +0530

    Initial commit

---
+++ Back-End/vyaparhead/src/main/java/com/vyaparmall/vyaparhead/VyaparheadApplication.java
@@ -0,0 +1,13 @@
+package com.vyaparmall.vyaparhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyaparheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyaparheadApplication.class, args);
+	}
+
+}
---
+++ Back-End/vyaparhead/src/test/java/com/vyaparmall/vyaparhead/VyaparheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyaparmall.vyaparhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyaparheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
