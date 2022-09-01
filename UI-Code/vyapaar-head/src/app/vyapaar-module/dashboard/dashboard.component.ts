2dfb2b7e751ef39f81151809fe89e06e4afce3cf
fire_heart <shivji1999@gmail.com>
2022-09-01 12:36:37 +0530

    Solved header Issue

--- UI-Code/vyapaar-head/src/app/vyapaar-module/dashboard/dashboard.component.ts
+++ UI-Code/vyapaar-head/src/app/vyapaar-module/dashboard/dashboard.component.ts
@@ -7,41 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  display: boolean=false
+  display: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
     const linkColor = document.querySelectorAll('.nav_link')
-    function colorLink(){
-          if(linkColor){
-          linkColor.forEach(l=> l.classList.remove('active'))
-          this.classList.add('active')
-          }
-          }
-          linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    function colorLink() {
+      if (linkColor) {
+        linkColor.forEach(l => l.classList.remove('active'))
+        this.classList.add('active')
+      }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+    this.showNavbar('header-toggle','nav-bar','body-pd','header')
   }
-  
 
-  showNavbar(toggleId, navId, bodyId, headerId){
-    
+
+  showNavbar(toggleId, navId, bodyId, headerId) {
+
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId)
 
-    
-    if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
+
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () => {
+        // show navbar
+        nav.classList.toggle('show')
+        // change icon
+        toggle.classList.toggle('bx-x')
+        // add padding to body
+        bodypd.classList.toggle('body-pd')
+        // add padding to header
+        headerpd.classList.toggle('body-pd')
+      })
     }
   }
 
