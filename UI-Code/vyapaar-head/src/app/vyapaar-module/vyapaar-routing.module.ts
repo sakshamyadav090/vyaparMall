c1c05ca6548ea6600972ff60567afc98acfc332d
pankaj <pankaj@faballey.com>
2022-09-06 22:02:17 +0530

    all conflict resolved and code work fine

--- UI-Code/vyapaar-head/src/app/vyapaar-module/vyapaar-routing.module.ts
+++ UI-Code/vyapaar-head/src/app/vyapaar-module/vyapaar-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomePageComponent } from './home-page/home-page.component';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home',component:HomePageComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'dashboard',component:DashboardComponent},
+  {path:'about-us',component:AboutUsComponent}
 ];
 
 @NgModule({
