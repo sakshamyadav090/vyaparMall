124e3fcce21251c2ae78ad36c08282402bf05d81
fire_heart <shivji1999@gmail.com>
2022-09-27 10:43:35 +0530

    Product Authorization

--- Back-End/twecom-product-service/src/main/java/com/twecom/service/ProductService.java
+++ Back-End/twecom-product-service/src/main/java/com/twecom/service/ProductService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.twecom.jwthelper.JwtTokenAuthorizer;
 import com.twecom.model.Product;
 import com.twecom.repository.ProductRepository;
 
@@ -15,28 +16,41 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repo;
 	
+	@Autowired
+	private JwtTokenAuthorizer authorizer;
+	
 	public List<Product> getAllProduct(){
-	 return repo.findAll();
+		return repo.findAll();
 	}
 	
-	public List<Product> getProductByUser(int id){
-		 return repo.findBypSupplierId(id);
+	public List<Product> getProductByUser(String token){
+		int userId = authorizer.isTokenValid(token);
+		 return repo.findBypSupplierId(userId);
 		}
 	
 	public Product getByProductId(int pId) {
 		return repo.findById(pId).get();
 	}
 	
-	public Product addProduct(Product p) {
+	public Product addProduct(Product p, String token) {
+		int userId = authorizer.isTokenValid(token);
+		p.setPSupplierId(userId);
 		return repo.save(p);
 	}
 
-	public Product updateProduct(Product p) {
-		p.setModifiedAt(new Date());
-		return repo.save(p);
+	public Product updateProduct(Product p, String token) {
+		int userId = authorizer.isTokenValid(token);
+		if(p.getPSupplierId()==userId) {
+			p.setModifiedAt(new Date());
+			return repo.save(p);
+		}else {
+			throw new RuntimeException("Product Not Found");
+		}
+		
 	}
 	
-	public String deleteProduct(int pId) {
+	public String deleteProduct(int pId, String token) {
+		int userId = authorizer.isTokenValid(token);
 		Product pr=repo.findById(pId).get();
 		//pr.setModifiedBy(pId);
 		pr.setIsDeleted(1);
