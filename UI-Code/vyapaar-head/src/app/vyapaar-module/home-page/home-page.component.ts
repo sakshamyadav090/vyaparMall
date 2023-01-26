695a62187ea9f1c9af17a76059a82b60326dfa87
ajpank <pkkushwaha8888@gmail.com>
2023-01-26 20:56:15 +0530

    added some code

--- UI-Code/vyapaar-head/src/app/vyapaar-module/home-page/home-page.component.ts
+++ UI-Code/vyapaar-head/src/app/vyapaar-module/home-page/home-page.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  images: any[]
   constructor() { }
 
   ngOnInit(): void {
+
+    this.images = [
+     {src: "../../../assets/Images/slide1.jpg",
+     alt: "test"},
+     {
+      src: "../../../assets/Images/slide3.jpg",
+      alt: "test"
+     } 
+    ]
   }
+
+  currentSlide = 0;
+
+  prevSlide() {
+    if (this.currentSlide === 0) {
+      this.currentSlide = this.images.length - 1;
+    } else {
+      this.currentSlide--;
+    }
+    console.log(this.currentSlide)
+  }
+
+  nextSlide() {
+    if (this.currentSlide === this.images.length - 1) {
+      this.currentSlide = 0;
+    } else {
+      this.currentSlide++;
+    }
+    console.log(this.currentSlide)
+  }
+
 
 }
