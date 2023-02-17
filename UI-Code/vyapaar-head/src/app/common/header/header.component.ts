84215c26fec56aebe295c9f6a5e1f7205586fd6b
Shivji1999 <shivji1999@gmail.com>
2023-02-17 11:10:07 +0530

    header changes

--- UI-Code/vyapaar-head/src/app/common/header/header.component.ts
+++ UI-Code/vyapaar-head/src/app/common/header/header.component.ts
@@ -1,4 +1,9 @@
+/// <reference types="jquery" />
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiUrls } from 'src/app/vyapaar-module/utilities/api-urls';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-header',
@@ -6,14 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  isUser: boolean;
-  constructor() { }
+  isUser: boolean = false;
+  categories: any [];
+  isLoggedIn: boolean=false;
+
+  constructor(private apiService: ApiService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.checkForUserLoggedInStatus();
+    this.getCategoryList();
 
     
-
   }
+
+  getCategoryList(){
+    this.apiService.list(ApiUrls.CATEGORY_LIST).subscribe(res=>{
+      this.categories=res.data;
+  },err=>{
+    console.log(err);
+  })
+  }
+
+  registerAsSupplier() {
+    this.router.navigate(['/auth/register'], { queryParams: { user: true } });
+  }
+
   
+  
+  checkForUserLoggedInStatus(){
+    if(localStorage.getItem('token')){
+      this.apiService.getWithoutId(ApiUrls.VERIFY_TOKEN).subscribe(res=>{
+        console.log(res);
+        if(res.success){
+          this.isLoggedIn = true;
+        }
+      },err=>{
+        console.log(err);
+      })
+    }else{
+      this.isLoggedIn = false;
+    }
+  }
 
 }
